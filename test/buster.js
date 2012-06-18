721a5bea5ca8d9ca55e42a335a43baced24e0572
DimitarChristoff <christoff@gmail.com>
2012-06-18 16:25:22 +0100

    added the mock sync response as an allowed resource for requesting later

--- test/buster.js
+++ test/buster.js
@@ -2,12 +2,15 @@ var config = exports;
 
 config['Browser tests'] = {
     rootPath: '../',
+
     environment: 'browser',
+
     libs: [
         'example/lib/mootools-core.js',
         // need ES5 shim for buster static and IE6/7/8
         'test/lib/es5-shim.min.js'
     ],
+
     sources: [
         // core
         'src/epitome.js',
@@ -21,8 +24,14 @@ config['Browser tests'] = {
         // extending extras
         'src/epitome-model-sync.js'
     ],
+
     tests: [
         // find matching test specs as above sources
         'test/tests/*-test.js'
+    ],
+
+    resources: [
+        // used as a static response json stub for model.sync
+        'example/data/1234-5123/*'
     ]
 };
