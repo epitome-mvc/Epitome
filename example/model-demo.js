8e44293488cd2fa41a67095a0d7f81fa91f31068
DimitarChristoff <christoff@gmail.com>
2012-06-19 11:42:33 +0100

    fixed broken demos

--- example/model-demo.js
+++ example/model-demo.js
@@ -1,5 +1,19 @@
-var m = new Epitome.Model({
-    foo: 'foo was here'
+// define a prototype for our model. You can just make an instance of Model but this is cleaner
+var testModel = new Class({
+
+    Extends: Epitome.Model,
+
+    options: {
+        defaults: {
+            foo: 'not bar'
+        }
+    }
+});
+
+
+
+var testInstance = new testModel({
+    initial: 'data'
 }, {
     onChange: function() {
         console.log(arguments);
@@ -10,5 +24,7 @@ var m = new Epitome.Model({
 });
 
 
-m.set('foo', 'bar');
-console.log(m.toJSON());
+// do some changes
+testInstance.set('foo', 'bar');
+
+console.log(testInstance.toJSON());
