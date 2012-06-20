ebdc4e7230b8295511f7821fb99fef6478ebb7e7
DimitarChristoff <christoff@gmail.com>
2012-06-20 10:45:44 +0100

    moved isEqual assertions out of model test

--- test/tests/epitome-model-test.js
+++ test/tests/epitome-model-test.js
@@ -1,53 +1,5 @@
 buster.testRunner.timeout = 1000;
 
-buster.testCase('Epitome.isEqual assertions >', {
-
-    'Expect no positives for objects with same length (bug in _.js)': function() {
-        buster.refute.isTrue(Epitome.isEqual({length: 330}, {length: 330, plays: 1}));
-    },
-
-    'Expect two identically looking objects to be equal': function() {
-        buster.assert.isTrue(Epitome.isEqual({length: 330}, {length: 330}));
-    },
-
-    'Expect two identically looking separate functions to not be equal': function() {
-        buster.refute.isTrue(Epitome.isEqual(
-            (function() { return 'hi';}),
-            (function() { return 'hi';})
-        ));
-    },
-
-    'Expect two references to the same function to be equal': function() {
-        var func = function() {
-            return 'hi';
-        };
-        buster.assert.isTrue(Epitome.isEqual(func, func));
-    },
-
-    'Expect two references to the same referenced function to be equal': function() {
-        var func = function() {
-            return 'hi';
-        }, func2 = func;
-        buster.assert.isTrue(Epitome.isEqual(func, func2));
-    },
-
-    'Expect two date objects to be equal': function() {
-        var date1 = new Date(2010, 6, 26, 0, 0, 0),
-            date2 = new Date(2010, 6, 26, 0, 0, 0);
-
-        buster.assert.isTrue(Epitome.isEqual(date1, date2));
-    },
-
-    '// Expect two different dates not to be equal': function() {
-        // this test fails! fucking hell.
-        var date1 = new Date(2010, 6, 26, 0, 0, 0),
-            date2 = new Date(2000, 6, 26, 0, 0, 0);
-
-        buster.refute.equals(Epitome.isEqual(date1, date2));
-    }
-
-});
-
 buster.testCase('Basic Epitome model creation with initial data >', {
     setUp: function() {
         this.dataInitial = {
