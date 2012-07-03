5ac4c42704771904c3f24cc560422f59f64820e9
DimitarChristoff <christoff@gmail.com>
2012-07-03 12:49:47 +0100

    reorganising view demos to split between bindings to model and collection

--- example/view-model-demo.js
+++ example/view-model-demo.js
@@ -4,17 +4,11 @@ var testView = new Class({
 	Extends: Epitome.View,
 
 	render: function() {
-		var views = [],
-			self = this;
-
 		var controls = this.element.getElement('div.task-controls').dispose();
 
 		this.empty();
-		this.collection.each(function(model) {
-			views.push(self.template(model.toJSON()));
-		});
 
-		this.element.set('html', views.join('')).adopt(controls);
+		this.element.set('html', this.template(this.model.toJSON())).adopt(controls);
 		this.parent();
 		return this;
 	}
@@ -24,83 +18,55 @@ var testView = new Class({
 
 var testModel = new Class({
 
-	Extends: Epitome.Model.Sync,
+	Extends: Epitome.Model,
 
 	options: {
 		defaults: {
-			urlRoot: '/blah'
+			id: 1,
+			title: 'Task one',
+			task: 'Do me'
 		}
 	}
 });
 
-var testCollectionProto = new Class({
-
-	Extends: Epitome.Collection,
-
-	model: testModel
-});
-
-var testCollection = new testCollectionProto([{
-	id: 1,
-	title: 'Task one',
-	task: 'Do me first'
-}, {
-	id: 2,
-	title: 'Task two',
-	task: 'Do me next'
-}, {
-	id: 3,
-	title: 'Task three',
-	task: 'Do me last'
-}]);
 
 
 var testInstance = new testView({
 
-	collection: testCollection,
+	model: new testModel(),
 
 	element: 'demo',
 
-	template: '<div class="task" data-id="{id}"><div class="floatRight"><a href="#" class="task-remove">x remove</a></div><div class="task-title">{title}</div><div class="task-body">{task}</div><div class="clear"></div></div>',
+	template: '<div class="task" data-id="{id}"><div class="floatRight"><a href="#" class="task-remove">x empty</a></div><div class="task-title">{title}</div><div class="task-body">{task}</div><div class="clear"></div></div>',
 
 	// event binding
 	events: {
-		'click:relay(a.task-remove)': 'removeTask',
-		'click:relay(button.task-create)': 'newTask',
-		'click:relay(button.change-one)': 'changeFirst'
+		'click:relay(a.task-remove)': 'emptyModel',
+		'click:relay(button.change-one)': 'changeModel'
 	},
 
 	onReady: function() {
 	},
 
-	'onChange:collection': function() {
+	'onChange:model': function() {
 		this.render();
 	},
 
-	onRemoveTask: function(e, el) {
+	onEmptyModel: function(e, el) {
 		e && e.stop && e.stop();
-		var id = el.getParent('div.task').get('data-id'),
-			model = this.collection.getModelById(id);
 
-		this.collection.removeModel(model);
+		this.model.empty();
 		this.render();
 	},
 
-	onNewTask: function(e, el) {
-		testCollection.addModel({
-			id: Number.random(1000, 100000),
-			title: 'New Task',
-			task: 'Some text'
-		});
-	},
+	onChangeModel: function(e, el) {
 
-	onChangeFirst: function(e, el) {
-		var model = testCollection.getRandom();
-		model.set({
-			title: String.uniqueID()
+		var model = this.model.toJSON();
+		Object.each(model, function(value, key) {
+			model[key] = String.uniqueID();
 		});
 
-		model.save();
+		this.model.set(model);
 	}
 });
 
