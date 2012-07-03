2d6b8956b7cca5e2d09bc5cfd3ca93dd86546afe
DimitarChristoff <christoff@gmail.com>
2012-07-03 16:07:31 +0100

    modded php data mocker for working with collections in testing

--- example/data/index.php
+++ example/data/index.php
@@ -1,16 +1,36 @@
 <?PHP
 
-$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '';
+header('Content-type: application/json');
 
-$response = Array(
-    'firstName' => Array('coda','This is the shit'),
-    'foobar' => 'No',
-    'foobar2' => 'Pre-populated',
-    'propertyType' => 'property_type_1',
-    'propertyTypeHouse' => 'Detatched house',
-    'id' => $id
-);
+function generateDummyData($count) {
+    $result = Array();
+    $id = rand(10000, 100000);
+    while($count--) {
+        $result[] = (object)array(
+            'name' => 'Dimitar',
+            'surname' => 'Christoff',
+            'foo' => 'bar',
+            'title' => 'Task #' . $id,
+            'task' => 'Task body for ' . $id,
+            'id' => $id
+        );
+        $id++;
+    }
+    return $result;
+}
 
-echo json_encode($response);
+if (isset($_REQUEST['a']) && $_REQUEST['a'] == 'collection') {
+    echo json_encode(generateDummyData(10));
+    die;
+}
+
+// do a single model quickly.
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : rand(100, 1000000);
+
+$response = generateDummyData(1);
+$response[0]->id = $id;
+unset($response[0]->title, $response[0]->task);
+
+echo json_encode($response[0]);
 
 ?>
