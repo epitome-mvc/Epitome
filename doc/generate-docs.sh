c4853ec021dd62b4df5983e6cb594871b7776280
DimitarChristoff <christoff@gmail.com>
2012-07-17 17:14:01 +0100

    repo for docs

--- doc/generate-docs.sh
+++ doc/generate-docs.sh
@@ -1,7 +1,7 @@
 if [ -z $(git status --porcelain) ];
 then
     echo "git status is clean. generating..."
-    curl -X POST --data-urlencode content@Epitome.md --data-urlencode name=Epitome --data-urlencode twitter=D_mitar --data-urlencode repo=DimitarChristoff/Epitome http://documentup.com/compiled > index.html && open index.html
+    curl -X POST --data-urlencode content@Epitome.md --data-urlencode name=Epitome --data-urlencode twitter=D_mitar --data-urlencode repo=DimitarChristoff/Epitome/ http://documentup.com/compiled > index.html && open index.html
 else
     echo "Please commit your changes first."
     git status
