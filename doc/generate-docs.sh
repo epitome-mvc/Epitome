ef54828992ad63b0dd1c8719a12be1f603c83f1c
DimitarChristoff <christoff@gmail.com>
2012-07-18 09:13:25 +0100

    clean gh pages

--- doc/generate-docs.sh
+++ doc/generate-docs.sh
@@ -2,8 +2,11 @@ if [ -z $(git status --porcelain) ];
 then
     echo "git status is clean. generating..."
     curl -X POST http://documentup.com/DimitarChristoff/Epitome/recompile/ > index.html
-    git checkout gh-pages
-    git commit -am'updating docs'
+    git br -D gh-pages
+    git push origin :gh-pages
+    git checkout --orphan gh-pages
+    git add index.html
+    git commit -m'updating docs'
     git push
 else
     echo "Please commit your changes first."
