295b27ab9a1eda7c52e0cbab6eb872b853780757
DimitarChristoff <christoff@gmail.com>
2012-07-18 09:07:46 +0100

    updated gen script

--- doc/generate-docs.sh
+++ doc/generate-docs.sh
@@ -2,6 +2,9 @@ if [ -z $(git status --porcelain) ];
 then
     echo "git status is clean. generating..."
     curl -X POST http://documentup.com/DimitarChristoff/Epitome/recompile/ > index.html
+    git checkout gh-pages
+    git commit -am'updating docs'
+    git push
 else
     echo "Please commit your changes first."
     git status
