f73459b1f554b94c4777dbe91b6cbf501a10ee69
DimitarChristoff <christoff@gmail.com>
2012-07-18 09:06:18 +0100

    updated gen script

--- doc/generate-docs.sh
+++ doc/generate-docs.sh
@@ -1,12 +1,7 @@
 if [ -z $(git status --porcelain) ];
 then
     echo "git status is clean. generating..."
-    curl -X POST --data-urlencode content@Epitome.md \
-    --data-urlencode name=Epitome \
-    --data-urlencode twitter=D_mitar \
-    --data-urlencode travis=true \
-    --data-urlencode "repo=git@github.com:DimitarChristoff/Epitome.git" \
-    http://documentup.com/compiled > index.html && open index.html
+    curl -X POST http://documentup.com/DimitarChristoff/Epitome/recompile/ > index.html
 else
     echo "Please commit your changes first."
     git status
