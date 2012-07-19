3775dc673f9c6391deddb072c8fe97deae70089a
DimitarChristoff <christoff@gmail.com>
2012-07-19 15:23:59 +0100

    making git checkout for gh page generation safer

--- generate-gh-pages.sh
+++ generate-gh-pages.sh
@@ -11,15 +11,19 @@ else
     git checkout gh-pages
     echo "--------------------------------------------------------------------------------"
     echo "running build.js..."
+
     ./build.js
+
     echo "--------------------------------------------------------------------------------"
     echo "adding index.html and pushing..."
 
     git add index.html
     git commit -m'automatically generated gh-pages'
     git push origin gh-pages
+
     echo "--------------------------------------------------------------------------------"
     echo "all done, going back to master now."
-    git co master
+
+    git checkout master
 fi
 echo "--------------------------------------------------------------------------------"
