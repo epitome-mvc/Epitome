7705bf20d2b2b3032957d744e7a4e2c55353fdee
DimitarChristoff <christoff@gmail.com>
2012-07-19 15:16:14 +0100

    page generator script

---
+++ generate-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "-----------------------------------------------------[gh-pages generator 0.0.1]-"
+if [ -n "$(git status --porcelain)" ]; then
+    echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
+    echo "--------------------------------------------------------------------------------"
+    git status
+else
+    echo "Current branch is CLEAN, checking out gh-pages..."
+    echo "--------------------------------------------------------------------------------"
+    git checkout gh-pages
+    echo "--------------------------------------------------------------------------------"
+    echo "running build.js..."
+    ./build.js
+    echo "--------------------------------------------------------------------------------"
+    echo "adding index.html and pushing...
+    git add index.html
+    git commit -m'automatically generated gh-pages'
+    git push origin gh-pages
+    echo "--------------------------------------------------------------------------------"
+    echo "all done, going back to master now.
+    git co master
+fi
+echo "--------------------------------------------------------------------------------"
