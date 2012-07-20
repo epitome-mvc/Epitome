73dd6438edee111dcb4d4def31426841623eafee
DimitarChristoff <christoff@gmail.com>
2012-07-20 11:09:57 +0100

    added colours to gh page build script

--- generate-gh-pages.sh
+++ generate-gh-pages.sh
@@ -1,41 +1,67 @@
 #!/usr/bin/env bash
 
-echo "-----------------------------------------------------[gh-pages generator 0.0.1]-"
-echo ""
-echo "                                                             ('.')"
-echo "                                                              <))=$"
-echo "                                                              _/\_"
-echo ""
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+MAGENTA='\033[35m'
+CYAN='\033[36m'
+WHITE='\033[37m'
+GRAY="\033[1;30m"
+LIGHT_GRAY="\033[37m"
+NO_COLOUR="\033[0m"
+
+LINE="$GRAY--------------------------------------------------------------------------------$NO_COLOUR"
+
+echo -e "$GRAY-------------------------------------------[$CYAN epitome gh-pages generator$YELLOW 0.0.2$GRAY ]-"
+echo -e ""
+echo -e "                                                             $YELLOW($BLUE'$MAGENTA.$BLUE'$YELLOW)"
+echo -e "                                                              $YELLOW<$WHITE))$YELLOW=$ "
+echo -e "                                                             $BLUE _/\_ $GRAY foo[tm]$NO_COLOUR"
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
-    echo "--------------------------------------------------------------------------------"
-    git status
+	echo -e $RED
+	echo -e "ERROR:$WHITE git status$RED was not clean. Please, commit your changes first."
+	echo -e $LINE
+
+	git status
+
+	echo -e $LINE
+	exit 1
 else
-    echo "Current branch is CLEAN, checking out gh-pages..."
-    echo "--------------------------------------------------------------------------------"
-    git checkout gh-pages
-    echo "--------------------------------------------------------------------------------"
-    echo "checking and getting dependencies..."
+    echo -e $GREEN
+	echo -e "Current branch is$WHITE clean$GREEN, checking out$CYAN gh-pages$GREEN..."
+	echo -e
+	git checkout gh-pages
 
-    npm install .
-    echo "--------------------------------------------------------------------------------"
-    echo "running build.js via npm run-script build..."
+	if [ -n "$(git status --porcelain)" ]; then
+		git reset --hard HEAD
+	fi
 
-    npm run-script build
-    rc=$?
+	echo -e $LINE
+	echo -e "Checking and getting dependencies via npm install..."
 
-    if [[ $rc != 0 ]]; then
-        echo "build.js has failed (exit code $rc). check logs and package.json, exiting..."
-        exit $rc
-    fi
+	npm install .
+	echo -e $LINE
+	echo -e "running$CYAN build.js$LIGHT_GRAY via$CYAN npm run-script build$NO_COLOUR..."
 
-    git commit -m'automatically generated gh-pages'
-    git push origin gh-pages
+	npm run-script build
+	rc=$?
 
-    echo "--------------------------------------------------------------------------------"
-    echo "all done, going back to master now."
+	if [[ $rc != 0 ]]; then
+		echo -e $RED
+		echo "ERROR: build.js has failed (exit code $rc). check logs and package.json, exiting..."
+		echo -e $LINE
+		exit $rc
+	fi
 
-    git checkout master
+	git add index.html
+	git commit -m'automatically generated gh-pages'
+	git push origin gh-pages
+
+	echo -e $LINE
+	echo -e "All done, going back to$CYAN master$NO_COLOUR now."
+
+	git checkout master
 fi
-echo "--------------------------------------------------------------------------------"
+echo -e $LINE
