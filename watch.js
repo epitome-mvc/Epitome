ab99d280eeaf0e5db9ba18a43181e25e6a738b89
DimitarChristoff <christoff@gmail.com>
2012-08-02 13:39:19 +0100

    watch.js auto builder now runs the tests first before producing Epitome-min.js

--- watch.js
+++ watch.js
@@ -2,12 +2,30 @@
 
 // recompile main minified file when sources change
 var fs = require('fs'),
-	ps = require('child_process');
+	ps = require('child_process'),
+	path = './src/';
 
-fs.watch('./src/', function (event, filename) {
+console.log('Watching for changes in ' + path);
 
-	ps.exec('r.js -o app.build.js', function(error, output) {
-		console.log(output);
+fs.watch(path, function (event, filename) {
+
+	console.log('File system change detected....');
+	console.log('Trying buster-test first, make sure buster-server is running and slaves are captured');
+	console.log('');
+
+	ps.exec('buster-test', function(error, stdout) {
+		if (!error) {
+			console.log('Tests finished OK, building a new release as per app.build.js...');
+			ps.exec('r.js -o app.build.js', function(error, output) {
+				console.log(output);
+			});
+		}
+		else {
+			console.log('The tests did not pass or failed to run. Not built.');
+		}
+		console.log('');
 	});
+
+
 
 });
