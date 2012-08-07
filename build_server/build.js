0d619758fe47699408a1ee9a319cb127b0005ca8
DimitarChristoff <christoff@gmail.com>
2012-08-07 13:20:42 +0100

    new ver of node build server via amd deps

--- build_server/build.js
+++ build_server/build.js
@@ -1,78 +1,112 @@
 #!/usr/bin/env node
 
-// recompile main minified file when sources change
-var fs = require('fs'),
+var requirejs = require('../node_modules/requirejs/bin/r.js'),
 	http = require('http'),
 	url = require('url'),
+	fs = require('fs'),
 	ps = require('child_process'),
-	path = '../src/',
+	host = '127.0.0.1',
 	port = 39170,
-	ip = '127.0.0.1',
-	modules = [
-		'epitome',
-		'epitome-isequal',
-		'epitome-model',
-		'epitome-model-sync',
-		'epitome-storage',
-		'epitome-collection',
-		'epitome-collection-sync',
-		'epitome-template',
-		'epitome-view',
-		'epitome-router'
-	],
-	build = {
+	config,
+	appBuild = ({
 		baseUrl: '../../src/',
 		optimize: 'uglify',
+		out:'./Epitome-min.js',
 		name:'epitome',
-		include: modules.slice()
-	};
+		include: [
+			'../build_server/hash/test.js'
+		]
+	}),
+	generateUID = function(uniqueIndex) {
+		var now = new Date();
+		return Math.floor(Math.random() * 10) + parseInt(now.getTime()).toString(36).toUpperCase();
+	},
+	id = generateUID(),
+	file = './hash/epitome-' + id + '.js';
 
+function respond(res, code, contents) {
+	res.writeHead(code, {
+		'Content-Type': (code === 200 ? 'application/javascript;charset=UTF-8' : 'text/plain'),
+		'Content-Length': contents.length
+	});
 
-var generateUID = function(uniqueIndex) {
-	var now = new Date();
-	return Math.floor(Math.random() * 10) + parseInt(now.getTime()).toString(36).toUpperCase();
-};
-
-
-console.log('Listening for build requests on http://' + ip + ':' + port + '/');
+	res.write(contents, 'utf8');
+	res.end();
+}
 
 http.createServer(function (req, res) {
-	var id = generateUID(),
-		file = './hash/build-' + id + '.json',
-		out = './out/epitome-' + id + '-min.js',
-		included = url.parse(req.url, true)['query'],
-		localBuild = JSON.parse(JSON.stringify(build));
+	var included = url.parse(req.url, true)['query'],
+		out = 'require([';
 
-	if (!included.build) {
-		console.log('Nothing requested, reverting to full build.');
-	}
-	else {
-		localBuild.include = included.build.split(',');
-	}
 
-	localBuild.out = '.' + out;
+	req.on('close', function (err) {
+		res.end();
+	});
 
-	fs.writeFile(file, '(' + JSON.stringify(localBuild) + ')', function(error) {});
+	req.on('end', function () {
+		// anything in the ?build= arg? needs to be comma separated.
+		if (included.build) {
+			// make a file that requires all
+			var deps = included.build.split(','),
+				depsArray = [];
 
-	console.log('Trying to generate build...');
-
-	ps.exec('r.js -o ' + file, function(error, output) {
-		if (error) {
-			console.log(error);
-		}
-
-		var fileName = error ? '../src/Epitome-min.js' : out;
-
-		fs.readFile(fileName, function(error, data) {
-			console.log('Read ' + fileName);
-
-			res.writeHead(200, {
-				'Content-Type': 'text/javascript'
+			deps.forEach(function(el) {
+				depsArray.push('"../../src/'+el+'"');
 			});
 
-			data = '/*Epitome hash: ' + id + ' */\n' + data;
-			res.end(data);
+			out += depsArray.join(',');
+			out += '], function(){});';
+
+			console.log('Created a custom include with ' deps.join(', '));
+
+			fs.writeFile(file, out, function(error) {});
+			appBuild.include = ['../build_server/hash/epitome-' + id];
+		}
+		else {
+			console.log('No custom includes found, returning main instead');
+
+			appBuild.include = ['main'];
+		}
+
+		// set output folder in out for quick reference
+		appBuild.out = '../out/epitome-'+ id +'-min.js';
+
+		fs.writeFile('./hash/' + id + '.json', JSON.stringify(appBuild), function() {
+			// what we will actually run now
+			console.log('running: r.js -o ./hash/' + id + '.json');
+
+			try {
+
+				ps.exec('r.js -o ./hash/' + id + '.json', function(error, output) {
+					console.log(output);
+
+					// remove the temporary module file, leave just config hash.
+					fs.unlink('./hash/epitome-' + id + '.js');
+
+
+					// if error, output 500 internal code with dump
+					if (error) {
+						respond(res, 500, error.toString());
+						return;
+					}
+
+					// read the generated file and pipe through to stdout (er, browser).
+					fs.readFile('./out/epitome-' + id + '-min.js', function(error, contents) {
+						// add a hash so same build config can be reused.
+						contents = '/*Epitome hash: ' + id + ' */\n' + contents;
+						respond(res, 200, contents);
+
+						// clean up the out file also, we can rebuild
+						fs.unlink('./out/epitome-' + id + '.js');
+					});
+				});
+			} catch (e) {
+				// something went wrong.
+				respond(res, 500, e.toString());
+			}
 		});
 	});
 
-}).listen(port, ip);
+}).listen(port, host);
+
+console.log('Server running at http://' + host + ':' + port + '/');
