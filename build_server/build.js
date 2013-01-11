45853654a99155f1e82716a422ba4360299051bf
DimitarChristoff <christoff@gmail.com>
2013-01-11 10:37:07 +0000

    tweaks to build.js for returning epitome builds

--- build_server/build.js
+++ build_server/build.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-var requirejs = require('../node_modules/requirejs/bin/r.js'),
-	http = require('http'),
+var http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
 	ps = require('child_process'),
@@ -9,31 +8,35 @@ var requirejs = require('../node_modules/requirejs/bin/r.js'),
 	host = '127.0.0.1',
 	port = 39170,
 	config,
+	greenOn = '\033[32m',
+	greenOff = '\033[39m',
+	redOn = '\033[31m',
+	redOff = greenOff,
 	appBuild = ({
 		baseUrl: '../../src/',
-		optimize: 'uglify',
-		out:'./Epitome-min.js',
-		name:'epitome',
+		optimize: 'uglify2',
+		out: './Epitome-min.js',
+		name: 'epitome',
 		include: [
-            'epitome',
-            'epitome-isequal',
-            'epitome-model',
-            'epitome-model-sync',
-            'epitome-storage',
-            'epitome-collection',
-            'epitome-collection-sync',
-            'epitome-template',
-            'epitome-view',
-            'epitome-router'
+			'epitome',
+			'epitome-isequal',
+			'epitome-model',
+			'epitome-model-sync',
+			'epitome-storage',
+			'epitome-collection',
+			'epitome-collection-sync',
+			'epitome-template',
+			'epitome-view',
+			'epitome-router'
 		]
 	}),
-	generateUID = function() {
+	generateUID = function(){
 		var now = new Date();
 		return Math.floor(Math.random() * 10) + parseInt(now.getTime()).toString(36).toUpperCase();
 	};
 
-function respond(res, code, contents) {
-	console.log('[LOG] ' + code);
+function respond(res, code, contents){
+	console.log('[' + greenOn + 'LOG' + greenOff +'] ' + code);
 	res.writeHead(code, {
 		'Content-Type': (code === 200 ? 'application/javascript;charset=UTF-8' : 'text/plain'),
 		'Content-Length': contents.length
@@ -43,42 +46,43 @@ function respond(res, code, contents) {
 	res.end();
 }
 
-http.createServer(function (req, res) {
-	var	u = url.parse(req.url, true),
+http.createServer(function(req, res){
+	var u = url.parse(req.url, true),
 		query = u['query'],
 		out = 'require([',
 		id = generateUID(),
 		file = './hash/epitome-' + id + '.js',
 		ip = req.connection.remoteAddress;
 
-	console.log('Started session for ' + ip);;
+	require('dns').reverse(ip, function(err, domains){
+		console.log('Started session for ' + greenOn + ip + greenOff + ' - ' + domains.join(' '));
+	});
 
-
-	req.on('close', function (err) {
+	req.on('close', function(err){
 		res.end();
 	});
 
-	req.on('end', function () {
+	req.on('end', function(){
 		var deps = [],
-			depsArray = [],
 			allowBuild = true;
 
 		// anything in the ?build= arg? needs to be comma separated.
-		if (query.build) {
+		if (query.build){
 			// make a file that requires all
 			deps = query.build.split(',');
 
 			console.log('Created a custom include with ' + deps.join(', '));
 
-			fs.writeFile(file, deps.join(','), function(error) {});
+			fs.writeFile(file, deps.join(','), function(error){
+			});
 			appBuild.include = deps;
 		}
-		else if (u.pathname != '/' && u.pathname.length == 10 && u.pathname.match(/\/([A-Z0-9]+)/)) {
+		else if (u.pathname != '/' && u.pathname.length == 10 && u.pathname.match(/\/([A-Z0-9]+)/)){
 			id = u.pathname.replace('/', '');
 			// see if the old build exists
 			var orig = './hash/epitome-' + id + '.js';
 
-			if (!path.existsSync(orig)) {
+			if (!path.existsSync(orig)){
 				respond(res, 404, 'Failed to find existing build for ' + id);
 				allowBuild = false;
 			}
@@ -93,56 +97,40 @@ http.createServer(function (req, res) {
 		}
 
 		// set output folder in out for quick reference
-		appBuild.out = '../out/epitome-'+ id +'-min.js';
+		appBuild.out = '../out/epitome-' + id + '-min.js';
 
-		var handleBuilding = function() {
-			console.log('[LOG] running: r.js -o ./hash/' + id + '.json');
+		var buildFile = './hash/' + id + '.json',
+			handleBuilding = function(){
+				ab = require(buildFile);
 
-			try {
-                // read original
-                fs.readFile('./hash/' + id + '.json', function(error, build) {
-                    var ab = JSON.parse(build);
+				console.log('['+greenOn+'LOG'+greenOff+'] running: r.js -o ' + file);
+
+				ps.exec('r.js -o ' + buildFile, function(error, output){
+					console.log(output);
+
+					// if error, output 500 internal code with dump
+					if (error){
+						respond(res, 500, error.toString());
+						return;
+					}
+
+					// read the generated file and pipe through to stdout (er, browser).
+					fs.readFile('./out/epitome-' + id + '-min.js', function(error, contents){
+						// add a hash so same build config can be reused.
+						contents = '/*Epitome hash: ' + id + '\n  Download: http://' + req.headers.host + '/' + id + '\n  Selected: ' + ab.include.join(', ') + ' */\n' + contents;
+						respond(res, 200, contents);
 
 
-                    ps.exec('r.js -o ./hash/' + id + '.json', function(error, output) {
-                        console.log(output);
+					});
+				});
+			};
 
-                        // if error, output 500 internal code with dump
-                        if (error) {
-                            respond(res, 500, error.toString());
-                            return;
-                        }
-
-
-
-                        // read the generated file and pipe through to stdout (er, browser).
-                        fs.readFile('./out/epitome-' + id + '-min.js', function(error, contents) {
-                            // add a hash so same build config can be reused.
-                            contents = '/*Epitome hash: ' + id + '\n  Download: http://' + req.headers.host + '/' + id +'\n  Selected: ' +  ab.include.join(', ') + ' */\n' + contents;
-                            respond(res, 200, contents);
-
-                            // clean up the out file also, we can rebuild
-                            /*fs.unlink('./out/epitome-' + id + '-min.js', function(error) {
-                                if (error)
-                                    throw error;
-                                console.log('deleted built js file');
-                            });*/
-                        });
-                    });
-                }); // json read
-			} catch (e) {
-				// something went wrong.
-				// respond(res, 500, e.toString());
-			}
-		};
-
-		fs.writeFile('./hash/' + id + '.json', JSON.stringify(appBuild), function() {
+		fs.writeFile(buildFile, JSON.stringify(appBuild), function(){
 			// what we will actually run now
-			if (allowBuild)
-				handleBuilding();
+			allowBuild && handleBuilding();
 		});
 	});
 
 }).listen(port);  // add host here to limit it
 
-console.log('Server running at http://' + host + ':' + port + '/');
+console.log(greenOn + 'Epitome' + greenOff + ' Build Server running at http://' + host + ':' + port + '/');
