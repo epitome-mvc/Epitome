04daabc2ec57e1b4e00516b6f19bb63aeb4845e6
DimitarChristoff <christoff@gmail.com>
2013-04-15 16:58:27 +0100

    more fixes of the new events model

--- src/epitome.js
+++ src/epitome.js
@@ -5,96 +5,143 @@
 	// wrapper function for requirejs or normal object
 	var wrap = function(){
 		// this is just the host object for the Epitome modules
-		Events.prototype.$subscribers = {};
 
+		// helpers
 		var removeOn = function(string){
 				return string.replace(/^on([A-Z])/, function(full, first){
 					return first.toLowerCase();
 				});
 			},
-			addEvent = function(type, fn, internal){
+
+			addEvent = function(type, fn){
 				type = removeOn(type);
 
 				this.$events[type] = (this.$events[type] || []).include(fn);
 				return this;
 			}.overloadSetter(),
+
 			removeEvent = function(type, fn){
 				// does not remove remote subscribers. careful, can cause memory issues if you don't clean up
 				type = removeOn(type);
 				var events = this.$events[type];
 				if (events){
-					var index = events.indexOf(fn);
-					if (index != -1) delete events[index];
+					if (fn){
+						var index = events.indexOf(fn);
+						if (index != -1) delete events[index];
+					}
+					else {
+						delete this.$events[type];
+					}
 				}
 				return this;
 			}.overloadSetter(),
+
 			all = '*',
-			EEvents = new Class({
 
-				Extends: Events,
+			undefined = 'undefined',
 
-				on: addEvent,
+			func = 'function';
 
-				off: removeEvent,
+		var EpitomeEvents = new Class({
+			// custom event implementation
 
-				trigger: function(type, args, delay){
-					type = removeOn(type);
-					var events = this.$events[type] || [],
-						subs = (type in this.$subscribers) ? this.$subscribers[type] : (all in this.$subscribers) ? this.$subscribers[all] : [],
-						self = this;
+			$events: {},
 
-					if (!events && !subs) return this;
-					args = Array.from(args);
+			$subscribers: {},
 
-					events.each(function(fn){
-						if (delay) fn.delay(delay, self, args);
-						else fn.apply(self, args);
-					});
+			on: addEvent,
 
-					subs.each(function(sub){
-						if (delay) sub.fn.delay(delay, sub.context, args);
+			off: removeEvent,
+
+			trigger: function(type, args){
+				type = removeOn(type);
+				var events = this.$events[type] || [],
+					subs = (type in this.$subscribers) ? this.$subscribers[type] : (all in this.$subscribers) ? this.$subscribers[all] : [],
+					self = this;
+
+				if (!events && !subs) return this;
+				args = Array.from(args);
+
+				events.each(function(fn){
+					// local events
+					fn.apply(self, args);
+				});
+
+				subs.each(function(sub){
+					// if event was added towards a specific callback, fire that
+					if (sub.fn){
 						sub.fn.apply(sub.context, args);
-					});
-
-					return this;
-				},
-
-				listenTo: function(obj, type, fn){
-					// obj: instance to subscribe to
-					// type: particular event type or all events, defaults to '*'
-					// last argument is the function to call, can shift to 2nd argument.
-					if (typeof type === 'function'){
-						fn = type;
-						type = all;
 					}
+					else {
+						// runs on subscriber, shifting arguments to pass on instance with a fake event object.
+						sub.subscriber.trigger(type, Array.combine([{
+							target: sub.context,
+							type: type
+						}], args));
+					}
+				});
 
-					obj.$subscribers[type] = (obj.$subscribers[type] || []).include({
+				return this;
+			},
+
+			listenTo: function(obj, type, fn){
+				// obj: instance to subscribe to
+				// type: particular event type or all events, defaults to '*'
+				// last argument is the function to call, can shift to 2nd argument.
+				var t = typeof type,
+					event = {
 						context: obj,
-						fn: fn
-					});
-				},
+						subscriber: this
+					};
 
-				stopListening: function(obj, type, fn){
-					// obj: instance to stop listening to
-					// type: particular event to unsubscribe from, or all events by default. '*' for wildcard events only
-					// fn: particular callback fn to unsubscribe from
-					var len;
-					Object.each(obj.$subscribers, function(value, key){
-						len = value.length;
-						if (typeof type !== 'undefined'){
-							if (key === type) while(len--)
-								(((fn && fn === value[len].fn) || !fn) && value[len].context === obj) && value.splice(len, 1);
-						}
-						else {
-							// no type, unsubscribe from all for that context object
-							while(len--) value[len].context === obj && value.splice(len, 1);
-						}
-					});
+				if (t === func){
+					fn = type;
+					type = all;
 				}
-			});
+				else if (t === undefined){
+					type = all;
+				}
 
-		var e = new EEvents();
-		e.Events = EEvents;
+				fn && (event.fn = fn);
+				obj.$subscribers[type] = (obj.$subscribers[type] || []).include(event);
+			},
+
+			stopListening: function(obj, type, fn){
+				// obj: instance to stop listening to
+				// type: particular event to unsubscribe from, or all events by default. '*' for wildcard events only
+				// fn: particular callback fn to unsubscribe from
+				var len;
+				Object.each(obj.$subscribers, function(value, key){
+					len = value.length;
+					if (typeof type !== undefined){
+						if (key === type) while(len--)
+							(((fn && fn === value[len].fn) || !fn) && value[len].context === obj) && value.splice(len, 1);
+					}
+					else {
+						// no type, unsubscribe from all for that context object
+						while(len--) value[len].context === obj && value.splice(len, 1);
+					}
+				});
+			},
+
+			setOptions: function(){
+				//refactored setOptions to use .on and not addEvent. auto-mixed in.
+				var options = this.options = Object.merge.apply(null, [{}, this.options].append(arguments)),
+					option;
+				for (option in options){
+					if (typeOf(options[option]) != 'function' || !(/^on[A-Z]/).test(option)) continue;
+					this.on(option, options[option]);
+					delete options[option];
+				}
+				return this;
+			}
+		});
+
+		// remove legacy methods
+
+		var e = new EpitomeEvents();
+		// export the constructor in the returned object
+		e.Events = EpitomeEvents;
 
 		return e;
 	};
